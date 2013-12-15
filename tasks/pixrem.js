5e80f2d57a70f3043339ac4ae381b641773b9283
Rob Wierzbowski <hello@robwierzbowski.com>
2013-12-15 13:51:45 -0500

    YOLO 80% of the plugin
    Most of the pixrem.js task adopted from grunt-contrib-uglfiy

--- tasks/pixrem.js
+++ tasks/pixrem.js
@@ -1,145 +1,66 @@
-/*global module*/
-'use strict';
-module.exports = function (grunt) {
-	var fs = require('fs');
-	var exec = require('child_process').exec;
+/*
+ * grunt-pixrem
+ * https://github.com/robwierzbowski/grunt-pixrem
+ *
+ * Copyright (c) 2013 Rob Wierzbowski
+ * Licensed under the MIT license.
+ */
 
-	grunt.registerMultiTask('jekyll', 'This triggers the `jekyll` command.', function () {
+'use strict';
+
+module.exports = function (grunt) {
+  var fs = require('fs');
+	var pixrem = require('pixrem');
+
+	grunt.registerMultiTask('pixrem', 'Generate pixel fallbacks for rem units.', function () {
 
 		var done = this.async();
-		var options = this.options();
-		var command = 'jekyll';
-		var optionList = {
-			'src': '--source',
-			'dest': '--destination',
-			'safe': '--safe',
-			'plugins': '--plugins',
-			'layouts': '--layouts',
-			'watch': '--watch',
-			'auto': '--watch',
-			'config': '--config',
-			'drafts': '--drafts',
-			'future': '--future',
-			'lsi': '--lsi',
-			'limit_posts': '--limit_posts',
-			'port': '--port',
-			'server_port': '--port',
-			'host': '--host',
-			'baseurl': '--baseurl',
-			'trace': '--trace',
+    var options = this.options({
+      rootvalue: 16,
+      replace: false
+    });
 
-			// Deprecated flags
-			'paginate': false,
-			'permalink': false,
-			'markdown': false,
-			'url': false
-		};
-		var majorVersion;
-		var rawConfigFile;
+    // Here below is straight up grunt uglify.
+    this.files.forEach(function (file) {
+      var output;
+      var src = file.src.filter(function (filepath) {
+        // Remove invalid source files
+        if (!grunt.file.exists(filepath)) {
+          grunt.log.warn('Source file "' + filepath + '" not found.');
+          return false;
+        }
+        else {
+          return true;
+        }
+      });
 
-		function testExists (next) {
-			var versionCommand = options.bundleExec ? 'bundle exec jekyll -v' : 'jekyll -v';
-			exec(versionCommand, function (error, stdout, stderr) {
+      if (src.length === 0) {
+        grunt.log.warn('skipped ' + file.dest + ' because src files were empty.');
+        return;
+      }
 
-				if (error) {
-					grunt.fail.warn('Please install Jekyll before running this task.');
-					done(false);
-				}
-				if (stdout) {
-					// Stdout returns `jekyll 1.x.x`, match returns first semver digit
-					majorVersion = stdout.match(/\d+/);
-					next();
-				}
-			});
-		}
+      try {
+        output = pixrem(src, options.rootvalue, { replace: options.replace });
+      }
+      catch (e) {
+        var err = new Error('Pixrem failed.');
+        console.log(e);
 
-		// Create temporary config file if needed
-		function configContext (next) {
-			if (options.raw) {
-				// Tmp file is only available within the context of this function
-				tmp.file({ prefix: '_config.', postfix: '.yml' }, function (err, path, fd) {
+        if (e.message) {
+          err.message += '\n' + e.message + '. \n';
+        }
 
-					rawConfigFile = path;
+        err.origError = e;
+        grunt.fail.warn(err);
+      }
 
-					if (err) {
-						grunt.fail.warn(err);
-					}
+       // Write the destination file.
+      grunt.file.write(file.dest, output);
 
-					// Write raw to file
-					fs.writeSync(fd, new Buffer(options.raw), 0, options.raw.length);
-					next();
-				});
-			}
-			else {
-				next();
-			}
-		}
+       // Print a success message.
+      grunt.log.writeln('File "' + file.dest + '" created.');
+    });
 
-		// Run configContext with command execution as a callback
-		function runJekyll (next) {
-
-			// Build the command string
-			if (options.bundleExec) {
-				command = 'bundle exec ' + command;
-			}
-
-			if (options.serve) {
-				command += majorVersion > 0 ? ' serve' : ' server';
-			}
-			else if (options.doctor) {
-				command += ' doctor';
-			}
-			else {
-				command += ' build';
-			}
-
-			// Insert temporary config path into the config option
-			if (typeof rawConfigFile != 'undefined') {
-				options.config = options.config ? options.config + ',' + rawConfigFile : rawConfigFile;
-			}
-
-			// Add flags to command if running serve or build
-			if (!options.doctor) {
-				Object.keys(optionList).forEach(function (option) {
-					if (options[option]) {
-						command += ' ' + optionList[option];
-						if (typeof options[option] !== 'boolean') {
-							command += ' ' + options[option];
-						}
-						if (!options[option]) {
-							grunt.warn('`' + option + '` has been deprecated. You may want to try this in the `raw` option in your gruntfile, or in a configuration file.');
-						}
-					}
-				});
-			}
-
-			// Execute command
-			grunt.log.write('`' + command + '` was initiated.\n');
-
-			if (options.serve) {
-				grunt.log.write('Started Jekyll web server on http://localhost:4000. Waiting...\n');
-			}
-
-			exec(command, function (err, stdout) {
-				grunt.log.write('\n\nJekyll output:\n' + stdout);
-
-				if (err) {
-					grunt.fail.warn(err);
-					done(false);
-				}
-				else {
-					next();
-				}
-			});
-		}
-
-		// Run the command
-		testExists(function() {
-			configContext (function() {
-				runJekyll(function() {
-					done(true);
-				});
-			});
-		});
+    done(true);
 	});
 };
